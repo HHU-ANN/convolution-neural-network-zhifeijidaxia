9ba8f9fb3ad4e94346013a004304acead7ce5f59
unknown <1146650581@qq.com>
2023-06-30 01:44:27 +0800

    第六次提交

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -55,7 +55,7 @@ def main():
     model = Digit()  # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
-    model.load_state_dict(torch.load(parent_dir+'/pth/model.pth'))
+    model.load_state_dict(torch.load(parent_dir+'/pth/model.pth', map_location='cpu'))
     #torch.load(model.state_dict(), 'pth/model.pth')
     #torch.load_state_dict('pth/model.pth')
     model.to('cpu')
